16af94dfd5d6963835d9c2259c95a924844b47c5
Andrea Grandi <a.grandi@gmail.com>
2015-11-27 20:39:18 +0000

    Add an integration test for PaymentService

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import pytest
+import time
 from mock import patch
 from nameko.testing.services import entrypoint_hook
-from paymentservice import PaymentConsumer
+from paymentservice import PaymentConsumer, PaymentService
 
 
 @patch('mandrill.Messages.send_template')
@@ -46,3 +47,28 @@ def test_payment_consumer_send_email_on_event(
                 'from_email': 'info@student.com'},
             template_content=[],
             template_name='payment-message')
+
+
+@patch('mandrill.Messages.send_template')
+def test_payment_consumer_integration(
+        send_template, container_factory, rabbit_config):
+    """
+        This is an integration test that voluntarily call a service that
+        will send the message through RabbitMQ and will verify if the
+        PaymentConsumer is able to read the message, parse it and call
+        the Mandrill API.
+    """
+    container_service = container_factory(PaymentService, rabbit_config)
+    container_service.start()
+
+    with entrypoint_hook(container_service, 'emit_event', None) as hook:
+        container_consumer = container_factory(PaymentConsumer, rabbit_config)
+        container_consumer.start()
+
+        # call the PaymentService
+        hook()
+
+        # Give the service the proper time to send the message to RabbitMQ
+        time.sleep(2)
+
+        assert send_template.called is True
